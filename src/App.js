6f0432b23a862141053073aa44f68fc52374da91
Vincent Tran <vincentuong@yahoo.com>
2022-03-30 12:09:37 -0500

    commit tree bb762823fba5b58b8ea99a7c0ff41a64e8255d13

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './App.css';
+import { addTodo } from  './actions/todos';
 
 class App extends Component {
 
@@ -17,7 +18,7 @@ class App extends Component {
   handleOnSubmit = event => {
     event.preventDefault();
     console.log("Todo being added: ", this.state.todo);
-    this.props.dispatch({ type: 'ADD_TODO', todo: this.state.todo });
+    this.props.addTodo(this.state.todo);
     this.setState({ todo: '' });
   }
 
@@ -41,10 +42,4 @@ class App extends Component {
   }
 };
 
-const mapStateToProps = (state) => {
-  return {
-    todos: state.todos
-  };
-};
-
-export default connect(mapStateToProps)(App);
+export default connect(state => ({ todos: state.todos }), { addTodo })(App);
